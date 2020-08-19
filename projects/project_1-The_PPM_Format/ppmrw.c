f0e1e2e0914a0ecc6c22133d700cbb8f8df5972e
bmeaz <brandonmeaz@gmail.com>
2020-08-19 14:41:27 -0700

    condensed code

--- projects/project_1-The_PPM_Format/ppmrw.c
+++ projects/project_1-The_PPM_Format/ppmrw.c
@@ -1,49 +1,48 @@
 #include "ppmrw.h"
 
 const int VALID_FORMS[] = {3, 6};   // Vaild types of forms accepted
+char PPM_IMAGE[4] = ".ppm";       // Valid type of input file
 
-char VALID_TYPES[4] = ".ppm"; // Valid type of input file
-
-////////////////////////////   inValidInput //////////////////////////////
+////////////////////////////   inValidInput  /////////////////////////////
 void fail (char *errMsg, const int errCode) {
-    
+
     fprintf(stderr, "Error: %s\n\n", errMsg);
-    
-    // if error caused by user input
+
+    // if error caused by stoopid user input
     if (errCode == IN_ERR_CODE) {
         printf("\tArguments should be: ./ppmrw <format> <input file> <output file>\n");
         printf("\t\tExample: ./ppmrw 6 input.ppm output.ppm\n\n");
     }
     exit(ERR_CODE);
-} 
+}
 
 
 ////////////////////////////   isValidInput  /////////////////////////////
 char *isValidInput (int form, char *input, char *output) {
     char *outStr;
     char msg[MAX_STR_LEN];
-    
-    
+
+
     // First Argument: incorrect form given
     if (!isValidForm(form)) {
        sprintf(msg, "Incorrect form value, This program can only use: P3 and P6");
     }
-    
+
     // Second Argument: file does not exist
     else if (!fileExists(input)) {
         sprintf(msg, "file '%s' cannot be found", input);
+    }
+
+    // Second Argument: incorrect type
+    else if(!isFileType(input, PPM_IMAGE)) {
+        sprintf(msg, "file '%s' is not a '%s' type", input, PPM_IMAGE);
      }
-     
-    // Second Argument:
-    else if(!isFileType(input, VALID_TYPES)) {
-        sprintf(msg, "file '%s' is not a '%s' type", input, VALID_TYPES);
-     }
-     
+
     // Third Argument: incorrect type
-    else if(!isFileType(output, VALID_TYPES)) {
-        sprintf(msg, "file '%s' is not a '%s' type", output, VALID_TYPES);
+    else if(!isFileType(output, PPM_IMAGE)) {
+        sprintf(msg, "file '%s' is not a '%s' type", output, PPM_IMAGE);
      }
-    
+
     outStr = msg;
     return outStr;
 }
@@ -53,7 +52,7 @@ char *isValidInput (int form, char *input, char *output) {
 bool isValidForm (int form)   {
      bool isValid = false;
      int numForms = sizeof(VALID_FORMS)/(VALID_FORMS[0]);
-     
+
      // loop through all valid forms
      for( int curForm = 0; curForm < numForms; curForm++ ) {
          // if form found, change flag to true
@@ -62,14 +61,14 @@ bool isValidForm (int form)   {
              isValid = true;
             }
      }
-     return isValid;    
+     return isValid;
 }
 
 ////////////////////////////   fileExists  ///////////////////////////////
 bool fileExists (char *filename) {
      bool fileExists = false;
      FILE *file = fopen(filename, "r");
-     
+
      // opens file and checks if it exists
      if (file != NULL){
          fileExists = true;
@@ -81,17 +80,8 @@ bool fileExists (char *filename) {
 
 ////////////////////////////   isFileType  ///////////////////////////////
 bool isFileType (char *filename, char *fileType) {
-     bool isValid = true;
-     int lastChar = strlen(filename)-1;
-     int typeLen = strlen(fileType)-1;
-     
-     // compare each char of the suffix of the filename to the correct filetype
-     for (int curChar = 0; curChar <= typeLen; curChar ++) {     
-         if (filename[lastChar-typeLen+curChar] != fileType[curChar]) {
-             isValid = false;
-         }
-     }
-     return isValid;
+     char *curFileType = &filename[strlen(filename) -strlen(fileType)];
+     return strcmp(curFileType, fileType) == 0;
 }
 
 ////////////////////////////   MAIN  ///////////////////////////////
@@ -99,10 +89,10 @@ int main (int argc, char *argv[]) {
     int form;       // user given form int
     char *input;    // user given input file
     char *output;   // user given output file
-    char validInput[MAX_STR_LEN]; 
+    char validInput[MAX_STR_LEN];
 
     // number of arguments is incorrect
-    if (argc != 4) { 
+    if (argc != 4) {
        fail("Invaild number of arguments", IN_ERR_CODE);
     }
     form = atoi(argv[1]);
@@ -114,9 +104,11 @@ int main (int argc, char *argv[]) {
     if (strcmp(validInput, EMPTY_STR) != 0) {
        fail(validInput, IN_ERR_CODE);
     }
-    
+
     printf("User has valid input");
-    
+
     return VALID_CODE;
 }
+
+
 
