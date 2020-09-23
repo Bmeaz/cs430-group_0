17f8b0b3e2d3af9881755d243a937220e583e067
Javier Quintana <44602186+JQuintana1997@users.noreply.github.com>
2020-09-22 18:53:09 -0700

    Add files via upload

---
+++ projects/project_2 - 3D Math/3dmath.c
@@ -0,0 +1,49 @@
+#include "3dmath.h"
+
+void v3_from_points(float *dst, float *a, float *b){
+
+}
+
+void v3_add(float *dst, float *a, float *b){
+
+}
+
+void v3_subtract(float *dst, float *a, float *b){
+
+}
+
+float v3_dot_product(float *dst, float *a, float *b){
+
+}
+
+void v3_cross_product(float *dst, float *a, float *b){
+
+}
+
+void v3_scale(float *dst, float s){
+
+}
+
+void v3_angle(float *dst, float *a, float *b){
+
+}
+
+void v3_angle_quick(float *dst, float *a, float *b){
+
+}
+
+void v3_reflect(float *dst, float *v, float *n){
+
+}
+
+float v3_length(float *a){
+
+}
+
+void v3_normalize(float *dst, float *a){
+
+}
+
+int main(int argc, char *argv[]){
+    
+}
