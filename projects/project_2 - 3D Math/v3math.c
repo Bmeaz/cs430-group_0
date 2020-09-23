cae20f583f666d1face768c97c5369ab2cc0288c
Bmeaz <brandonmeaz@gmail.com>
2020-09-23 16:49:06 -0700

    Added methods to project 2

--- projects/project_2 - 3D Math/v3math.c
+++ projects/project_2 - 3D Math/v3math.c
@@ -1,43 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
 #include "v3math.h"
 
-void v3_from_points(float *dst, float *a, float *b)
-{
+void v3_from_points(float *dst, float *a, float *b) {
     dst[0] = b[0] - a[0];
     dst[1] = b[1] - a[1];
     dst[2] = b[2] - a[2];
 }
 
-void v3_add(float *dst, float *a, float *b)
-{
+void v3_add(float *dst, float *a, float *b) {
     dst[0] = a[0] + b[0];
     dst[1] = a[1] + b[1];
     dst[2] = a[2] + b[2];
-
 }
 
-void v3_subtract(float *dst, float *a, float *b)
-{
+void v3_subtract(float *dst, float *a, float *b) {
     dst[0] = a[0] - b[0];
     dst[1] = a[1] - b[1];
     dst[2] = a[2] - b[2];
 }
 
-float v3_dot_product(float *a, float *b)
-{
-    float result1;
-    float result2;
-    float result3;
-
-    result1 = a[0]*b[0];
-    result2 = a[1]*b[1];
-    result3 = a[2]*b[2];
-
-    return result1+result2+result3;
+float v3_dot_product(float *a, float *b) {
+    return (a[0]*b[0]) + (a[1]*b[1]) + (a[2]*b[2]);
 }
+
+void v3_cross_product(float *dst, float *a, float *b) {
+    dst[0] = (a[1] * b[2]) - (a[2] * b[1]);
+    dst[1] = (a[2] * b[0]) - (a[0] * b[2]);
+    dst[2] = (a[0] * b[1]) - (a[1] * b[0]);
+}
+
+void v3_scale(float *dst, float s) {
+    dst[0] = s * dst[0];
+    dst[1] = s * dst[1];
+    dst[2] = s * dst[2];
+}
+
+
+void v3_angle(float *dst, float *a, float *b) {
+}
+
+
+void v3_angle_quick(float *dst, float *a, float *b) {
+
+}
+
+void v3_reflect(float *dst, float *v, float *n) {
+
+}
+
+float v3_length(float *a){
+    return sqrtf((a[0] * a[0]) + (a[1] * a[1]) + (a[2] * a[2]));
+}
+
+void v3_normalize(float *dst, float *a) {
+
+}
+
+bool v3_equals(float *a, float *b, float tolerance) {
+    float diff;
+    v3_subtract(&diff, a, b);
+    if(fabs(diff) > tolerance) {
+        return false;
+    }
+    return true; 
+}
+
+
+int main(int argc, char *argv[]) 
+{
+    return 0;
+}
+
--- projects/project_2 - 3D Math/v3math.h
+++ projects/project_2 - 3D Math/v3math.h
@@ -1,5 +1,5 @@
-#ifndef 3DMATH_H_INCLUDE
-#define 3DMATH_H_INCLUDE
+#ifndef V3MATH_H_INCLUDE
+#define V3MATH_H_INCLUDE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,30 +7,32 @@
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-void v3_from_points(float *dst, float *a, float *b);
+void v3_from_points (float *dst, float *a, float *b);
 
-void v3_add(float *dst, float *a, float *b);
+void v3_add (float *dst, float *a, float *b);
 
-void v3_subtract(float *dst, float *a, float *b);
+void v3_subtract (float *dst, float *a, float *b);
 
-float v3_dot_product(float *dst, float *a, float *b);
+float v3_dot_product (float *a, float *b);
 
-void v3_cross_product(float *dst, float *a, float *b);
+void v3_cross_product (float *dst, float *a, float *b);
 
-void v3_scale(float *dst, float s);
+void v3_scale (float *dst, float s);
 
-void v3_angle(float *dst, float *a, float *b);
+void v3_angle (float *dst, float *a, float *b);
 
-void v3_angle_quick(float *dst, float *a, float *b);
+void v3_angle_quick (float *dst, float *a, float *b);
 
-void v3_reflect(float *dst, float *v, float *n);
+void v3_reflect (float *dst, float *v, float *n);
 
-float v3_length(float *a);
+float v3_length (float *a);
 
 void v3_normalize(float *dst, float *a);
 
-int main(int argc, char *argv[]);
+bool v3_equals (float *a, float *b, float tolerance);
 
+int main (int argc, char *argv[]);
 
 #endif
