66fa345e53eda0f3d17da2724003793fb7a4e12b
Javier Quintana <44602186+JQuintana1997@users.noreply.github.com>
2020-09-26 13:35:34 -0700

    Add files via upload

---
+++ projects/project_2 - 3D Math/v3test.h
@@ -0,0 +1,36 @@
+#ifndef V3TEST_H_INCLUDE
+#define V3TEST_H_INCLUDE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+void v3_from_points_test (float *dst, float *a, float *b);
+
+void v3_add_test (float *dst, float *a, float *b);
+
+void v3_subtract_test (float *dst, float *a, float *b);
+
+float v3_dot_product_test (float *a, float *b);
+
+void v3_cross_product_test (float *dst, float *a, float *b);
+
+void v3_scale_test (float *dst, float s);
+
+void v3_angle_test (float *dst, float *a, float *b);
+
+void v3_angle_quick_test (float *dst, float *a, float *b);
+
+void v3_reflect_test (float *dst, float *v, float *n);
+
+float v3_length_test (float *a);
+
+void v3_normalize_test (float *dst, float *a);
+
+int main (int argc, char *argv[]);
+
+#endif
