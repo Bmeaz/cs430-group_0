5bf415896e85c68f0b34009d1d63491d7df6dd36
Javier Quintana <44602186+JQuintana1997@users.noreply.github.com>
2020-09-26 19:52:19 -0700

    Update v3test.c
    Just adding what I could do at work (which is not much)

--- projects/project_2 - 3D Math/v3test.c
+++ projects/project_2 - 3D Math/v3test.c
@@ -1,2 +1,49 @@
-#include "v3math.h"
+#include "v3test.h"
 
+void v3_from_points_test (float *dst, float *a, float *b){
+
+}
+
+void v3_add_test (float *a, float *b){
+    testArray
+}
+
+void v3_subtract_test (float *a, float *b){
+
+}
+
+float v3_dot_product_test (float *a, float *b){
+
+}
+
+void v3_cross_product_test (float *dst, float *a, float *b){
+
+}
+
+void v3_scale_test (float s){
+    
+}
+
+void v3_angle_test (float *a, float *b){
+
+}
+
+void v3_angle_quick_test (float *a, float *b){
+
+}
+
+void v3_reflect_test (float *dst, float *v, float *n){
+
+}
+
+float v3_length_test (float *a){
+
+}
+
+void v3_normalize_test (float *dst, float *a){
+
+}
+
+int main (int argc, char *argv[]){
+    return 0;
+}
