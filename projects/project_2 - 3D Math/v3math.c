cdcb69ddc71490e63542483d2cd976f04abd9dd9
Javier Quintana <44602186+JQuintana1997@users.noreply.github.com>
2020-09-27 23:15:07 -0700

    Update v3math.c
    Added the angle quick function

--- projects/project_2 - 3D Math/v3math.c
+++ projects/project_2 - 3D Math/v3math.c
@@ -46,7 +46,28 @@ void v3_angle(float *a, float *b) {
 }
 
 
-void v3_angle_quick(float *dst, float *a, float *b) {
+// angle between a and b; no cos-1
+void v3_angle_quick(float *a, float *b) {
+    float A[] = {0.0, 0.0, 0.0};
+    float B[] = {0.0, 0.0, 0.0};
+    // inverse cosine x=y
+    v3_normalize(A, a);
+    v3_normalize(B, b);
+
+    // Check to see if any values are equal to each other, if so pick other
+    // values for B
+    if(A[0] == B[0] || A[0] == B[1] || A[0] == B[2])
+    {
+        v3_normalize(B,b);
+    }
+    if(A[1] == B[0] || A[1] == B[1] || A[1] == B[2])
+    {
+        v3_normalize(B,b);
+    }
+    if(A[2] == B[0] || A[2] == B[1] || A[2] == B[2])
+    {
+        v3_normalize(B,b);
+    }
 
 }
 
