123050fc40c710d047ffdb5d04627598b1869e13
StevenSprouls <49702096+StevenSprouls@users.noreply.github.com>
2020-10-25 20:20:33 -0700

    Javier's branch

--- projects/project_5-Ray_Tracing/v3math.c
+++ projects/project_5-Ray_Tracing/v3math.c
@@ -10,12 +10,23 @@ void setArray(float *dst, float *a) {
     dst[2] = a[2];
 
 }
- 
-void v3_multiply(float *dst, float *a, float *b) {
-    dst[0] = a[0] * b[0];
-    dst[1] = a[1] * b[1];
-    dst[2] = a[2] * b[2];
+
+void get_light_vector(float *dst, float *o, float *n) {
+    float numerator[3] = {0,0,0};
+    float denom[3] = {0,0,0};
+    v3_subtract(numerator, o, n);
+    setArray(denom, numerator);
+    v3_normalize(denom, denom);
+    v3_divide(dst, numerator, denom);
 }
+    
+void v3_divide(float *dst, float *a, float *b) {
+    for (int x = 0; x < 3; x ++) {
+        assert(b[x] != 0);
+        dst[x] = a[x] / b[x];
+    }
+}
+    
     
 void v3_from_points(float *dst, float *a, float *b) {
     dst[0] = b[0] - a[0];
@@ -66,22 +77,23 @@ float v3_angle_quick(float *a, float *b){
 }
 
 
+// From notes in class:
+// Vr = v + 2b
+// b = -(n*v) * n
+// Vr = v + -2 (n*v) * n
 void v3_reflect(float *dst, float *v, float *n) {
-    float prod[3] = {0,0,0};
-    float b = v3_dot_product(v, n) * 2;
-    setArray(prod, v);
-    v3_scale(n, b);
-	v3_normalize(prod, prod); // ADDED LINE
-    v3_subtract(dst, v, prod);
+    /*    float val = v3_dot_product(v, n);
+    for (int x = 0; x < 3; x++) {
+        dst[x] = 2 * val * n[x] - v[x];
+    } */
+    for(int refIndex = 0; refIndex < 3; refIndex++){
+        // Compute b at the start of the loop
+        float b = -((n[refIndex])*(v[refIndex])) * (n[refIndex]);
+        //Compute Vr by using previously calculated b
+        dst[refIndex] = v[refIndex] + (2*b);
+    }
 
-	/*
-    float prod[3] = {0,0,0};
-    float b = v3_dot_product(v, n) * 2;
-    setArray(prod, v);
-    v3_scale(prod, b);
-    v3_normalize(n, v);
-    v3_subtract(dst, v, prod);
-	*/
+
 }
 
 float v3_length(float *a){
